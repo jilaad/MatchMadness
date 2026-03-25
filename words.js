aa10bc03e6bd98e4953a9f9b520d9621a2171d30
Victor Wilcox <vnwilcox4@gmail.com>
2026-03-24 21:25:03 -0400

    initial card setup

---
+++ words.js
@@ -0,0 +1,12 @@
+const words = [
+    { word: "cajón", image: "images/cajon.jpg" },
+    { word: "horno", image: "images/horno.jpg" },
+    { word: "mariposa", image: "images/mariposa.jpg" },
+    { word: "perro", image: "images/perro.jpg" },
+    { word: "puerta", image: "images/puerta.jpg" },
+    { word: "silla", image: "images/silla.jpg" },
+    { word: "sol", image: "images/sol.jpg" },
+    { word: "taza", image: "images/taza.jpg" },
+    { word: "tenedor", image: "images/tenedor.jpg" },
+    { word: "vaso", image: "images/vaso.jpg" }
+];
