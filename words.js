8188c9fddff31f067a613f8e217b29bfac3fedbf
Nate <vnwilcox4@gmail.com>
2026-04-03 00:05:57 -0400

    More word bank

--- words.js
+++ words.js
@@ -1,49 +1,88 @@
 const words = [
     { word: "cajón", type: "noun", images: [
-        { file: "images/cajon.jpg"}
+        { file: "images/drawer01.jpg" },
+        { file: "images/drawer02.jpg" },
+        { file: "images/drawer03.jpg" },
+        { file: "images/drawer04.jpg" }
     ]},
     { word: "horno", type: "noun", images: [
-        { file: "images/horno.jpg" }
+        { file: "images/oven01.jpg" },
+        { file: "images/oven02.jpg" },
+        { file: "images/oven03.jpg" },
+        { file: "images/oven04.jpg" }
     ]},
     { word: "mariposa", type: "noun", images: [
-        { file: "images/mariposa.jpg" }
+        { file: "images/butterfly01.jpg" },
+        { file: "images/butterfly02.jpg" },
+        { file: "images/butterfly03.jpg" },
+        { file: "images/butterfly04.jpg" }
     ]},
     { word: "perro", type: "noun", images: [
-        { file: "images/perro.jpg" }
+        { file: "images/dog01.jpg" },
+        { file: "images/dog02.jpg" },
+        { file: "images/dog03.jpg" },
+        { file: "images/dog04.jpg" }
     ]},
     { word: "puerta", type: "noun", images: [
-        { file: "images/puerta.jpg" }
+        { file: "images/door01.jpg" },
+        { file: "images/door02.jpg" },
+        { file: "images/door03.jpg" },
+        { file: "images/door04.jpg" }
     ]},
     { word: "silla", type: "noun", images: [
-        { file: "images/silla.jpg" }
+        { file: "images/chair01.jpg" },
+        { file: "images/chair02.jpg" },
+        { file: "images/chair03.jpg" },
+        { file: "images/chair04.jpg" }
     ]},
     { word: "sol", type: "noun", images: [
-        { file: "images/sol.jpg" }
+        { file: "images/sun01.jpg" },
+        { file: "images/sun02.jpg" },
+        { file: "images/sun03.jpg" },
+        { file: "images/sun04.jpg" }
     ]},
     { word: "taza", type: "noun", images: [
-        { file: "images/taza.jpg" }
+        { file: "images/mug01.jpg" },
+        { file: "images/mug02.jpg" },
+        { file: "images/mug03.jpg" },
+        { file: "images/mug04.jpg" }
     ]},
     { word: "tenedor", type: "noun", images: [
-        { file: "images/tenedor.jpg" }
+        { file: "images/fork01.jpg" },
+        { file: "images/fork02.jpg" },
+        { file: "images/fork03.jpg" },
+        { file: "images/fork04.jpg" }
     ]},
     { word: "vaso", type: "noun", images: [
-        { file: "images/vaso.jpg" },
-        { file: "images/Glass-of-water.jpg", accepts: ["agua","vaso"]}
+        { file: "images/drinking_glass01.jpg" },
+        { file: "images/drinking_glass02.jpg" },
+        { file: "images/drinking_glass03.jpg" },
+        { file: "images/plastic_cup01.jpg" },
+        { file: "images/plastic_cup02.jpg" },
+        { file: "images/paper_cup01.jpg" }
     ]},
-    { word: "agua", images: [
-        { file: "images/Glass-of-water.jpg", accepts: ["agua","vaso"]},
-        { file: "images/water.jpg"}
+    { word: "agua", type: "noun", images: [
+        { file: "images/water01.jpg" },
+        { file: "images/water02.jpg" },
+        { file: "images/water03.jpg" },
+        { file: "images/water04.jpg" }
     ]},
     { word: "tarjeta de crédito", type: "noun", images: [
-        { file: "images/credit_card.jpg" }
+        { file: "images/credit_card01.jpg" },
+        { file: "images/credit_card02.jpg" },
+        { file: "images/credit_card03.jpg" }
     ]},
     { word: "camarero", type: "noun", images: [
-        { file: "images/waiter.jpg", accepts: ["camarero","mesero"]},
-        { file: "images/waiter02.jpg", accepts: ["camarero","mesero"]}
+        { file: "images/waiter01.jpg", accepts: ["camarero","mesero"]},
+        { file: "images/waiter02.jpg", accepts: ["camarero","mesero"]},
+        { file: "images/waiter03.jpg", accepts: ["camarero","mesero"]},
+        { file: "images/waiter04.jpg", accepts: ["camarero","mesero"]}
     ]},
     { word: "mesero", type: "noun", images: [
-        { file: "images/waiter.jpg", accepts: ["camarero","mesero"]},
-        { file: "images/waiter02.jpg", accepts: ["camarero","mesero"]}
+        { file: "images/waiter01.jpg", accepts: ["camarero","mesero"]},
+        { file: "images/waiter02.jpg", accepts: ["camarero","mesero"]},
+        { file: "images/waiter03.jpg", accepts: ["camarero","mesero"]},
+        { file: "images/waiter04.jpg", accepts: ["camarero","mesero"]}
     ]},
     { word: "mesa", type: "noun", images: [
         { file: "images/table01.jpg" },
@@ -111,10 +150,8 @@ const words = [
         { file: "images/bottle04.jpg" }
     ]},
     { word: "bandeja", type: "noun", images: [
-        { file: "images/tray01.jpg" },
         { file: "images/tray02.jpg" },
-        { file: "images/tray03.jpg" },
-        { file: "images/tray04.jpg" }
+        { file: "images/tray03.jpg" }
     ]},
     { word: "propina", type: "noun", images: [
         { file: "images/tip_money01.jpg" },
@@ -198,9 +235,7 @@ const words = [
         { file: "images/turn_on01.jpg" },
         { file: "images/turn_on02.jpg" },
         { file: "images/turn_on03.jpg" },
-        { file: "images/turn_on04.jpg" }
-    ]},
-    { word: "encender", type: "verb", images: [
+        { file: "images/turn_on04.jpg" },
         { file: "images/lighting_like_fire01.jpg" },
         { file: "images/lighting_like_fire02.jpg" },
         { file: "images/lighting_like_fire03.jpg" },
@@ -217,5 +252,22 @@ const words = [
         { file: "images/sirloin02.jpg" },
         { file: "images/sirloin03.jpg" },
         { file: "images/sirloin04.jpg" }
+    ]},
+    { word: "ajo", type: "noun", images: [
+        { file: "images/garlic01.jpg" },
+        { file: "images/garlic02.jpg" },
+        { file: "images/garlic03.jpg" }
+    ]},
+    { word: "cebolla", type: "noun", images: [
+        { file: "images/onion01.jpg" },
+        { file: "images/onion02.jpg" },
+        { file: "images/onion03.jpg" },
+        { file: "images/onion04.jpg" }
+    ]},
+    { word: "mantequilla", type: "noun", images: [
+        { file: "images/butter01.jpg" },
+        { file: "images/butter02.jpg" },
+        { file: "images/butter03.jpg" },
+        { file: "images/butter04.jpg" }
     ]}
 ];
